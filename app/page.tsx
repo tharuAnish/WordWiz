5f0addacaef79b44e73c6f61190314d2c39c3d97
tharuAnish <namoanishtharu@gmail.com>
2024-05-18 21:17:28 +0530

    converter created

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Converter from "@/components/converter/converter"
 import Nav from "@/components/nav/nav"
 import { ModeToggle } from "@/components/theme/toggle"
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <Nav />
+      <Converter />
     </div>
   )
 }
